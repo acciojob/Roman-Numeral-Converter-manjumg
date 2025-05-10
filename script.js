c37a8e3899fd90735268430c0086d47fac9ed864
Manjunath M Goni <manjugoni007@gmail.com>
2025-05-10 11:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+function convertToRoman() {
+  const num = parseInt(document.getElementById('numberInput').value);
+  const resultEl = document.getElementById('result');
 
-  //your code here
+  if (isNaN(num) || num < 1 || num > 100000) {
+    resultEl.textContent = 'Please enter a number between 1 and 100000.';
+    return;
+  }
 
+  const romanNumerals = [
+    ['ↈ', 100000],
+    ['ↇ', 50000],
+    ['ↂ', 10000],
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+
+  let result = '';
+  let number = num;
+
+  for (const [symbol, value] of romanNumerals) {
+    while (number >= value) {
+      result += symbol;
+      number -= value;
+    }
+  }
+
+  resultEl.textContent = result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
